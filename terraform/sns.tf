e440ca8ed2607c61d70f6797b393f0131b93733e
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2026-04-22 13:42:23 +0200

    added orderconfirmation topic subscription

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,3 +1,9 @@
 resource "aws_sns_topic" "order-confirmation" {
   name = "order-confirmation-topic"
 }
+
+resource "aws_sns_topic_subscription" "order-confirmation-email" {
+  topic_arn = aws_sns_topic.order-confirmation.arn
+  protocol  = "email"
+  endpoint  = "user@example.com" ## Replace with actual email address
+}
