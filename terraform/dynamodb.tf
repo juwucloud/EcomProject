ddab6259c275f69cf50d97fbbf7c6fb3311382cf
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2026-04-22 12:00:26 +0200

    add dynamodb table with primary key 'order_id'

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,10 @@
+resource "aws_dynamodb_table" "orders" {
+  name           = "orders"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "order_id"
+
+  attribute {
+    name = "order_id"
+    type = "S"
+  }
+}
