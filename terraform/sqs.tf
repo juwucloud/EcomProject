82eec2042cc89f73a8701cbd8dcf2432277333ed
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2026-04-22 12:58:19 +0200

    added orderprocessor dlq + orderprocessor sqs

---
+++ terraform/sqs.tf
@@ -0,0 +1,21 @@
+## Order Processor DEAD LETTER QUEUE
+
+resource "aws_sqs_queue" "order_processor_dlq" {
+  name = "order-processor-dlq"
+}
+
+## Order Processor Queue
+
+resource "aws_sqs_queue" "order_processor_queue" {
+  name = "order-processor-queue"
+  visibility_timeout_seconds = 60
+  message_retention_seconds = 86400
+
+  redrive_allow_policy = jsonencode({
+    maxReceiveCount = 3
+    deadLetterTargetArn = aws_sqs_queue.order_processor_dlq.arn
+  })
+}
+
+
+
