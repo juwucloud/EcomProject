2c90d9818cf45eb44dc7edb4b79e16d850c92494
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2026-04-22 16:17:57 +0200

    added cloudfront with access to s3 OAC

--- terraform/s3_cloudfront.tf
+++ terraform/s3_cloudfront.tf
@@ -32,3 +32,78 @@ resource "aws_s3_object" "html" {
   content_type = "text/html"
   etag         = filemd5("${path.module}/../Frontend/sneaker-shop.html")
 }
+
+
+# CloudFront Origin Access Control (OAC) 
+resource "aws_cloudfront_origin_access_control" "website" {
+  name                              = "ecommerce-website-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "website" {
+  enabled             = true
+  default_root_object = "sneaker-shop.html"
+  price_class         = "PriceClass_100" # Nur US/EU (günstiger)
+
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "S3-${aws_s3_bucket.website.id}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.website.id
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-${aws_s3_bucket.website.id}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# S3 Bucket Policy - allows CloudFront access
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.website.arn
+          }
+        }
+      }
+    ]
+  })
+}
