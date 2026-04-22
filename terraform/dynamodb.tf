ce43b06f87478c394d27fb62dd0d744f4383cc92
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2026-04-22 13:10:08 +0200

    fmt

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "orders" {
-  name           = "orders"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "order_id"
+  name         = "orders"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "order_id"
 
   attribute {
     name = "order_id"
--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -7,12 +7,12 @@ resource "aws_sqs_queue" "order_processor_dlq" {
 ## Order Processor Queue
 
 resource "aws_sqs_queue" "order_processor_queue" {
-  name = "order-processor-queue"
+  name                       = "order-processor-queue"
   visibility_timeout_seconds = 60
-  message_retention_seconds = 86400
+  message_retention_seconds  = 86400
 
   redrive_allow_policy = jsonencode({
-    maxReceiveCount = 3
+    maxReceiveCount     = 3
     deadLetterTargetArn = aws_sqs_queue.order_processor_dlq.arn
   })
 }
