13287867fa04ad78254a6c49ead0e6bcb4fba5a7
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2026-04-22 13:23:27 +0200

    added backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "juwu-terraform-state-bucket"
+    bucket = "juwu-terraform-state-bucket". ## custom name
     key    = "ecommerce/terraform.tfstate"
     region = "eu-central-1"
   }
