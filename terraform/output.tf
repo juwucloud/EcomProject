773e4e08dd88db56eb6dfb7ee9b78168d1861afb
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2026-04-22 15:23:42 +0200

    added outputs to output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,14 @@
+output "api_url" {
+  description = "API Gateway URL for order endpoint"
+  value       = "${aws_api_gateway_stage.prod.invoke_url}/order"
+}
+
+output "dynamodb_table_name" {
+  description = "DynamoDB Orders Table Name"
+  value       = aws_dynamodb_table.orders.name
+}
+
+output "sns_topic_arn" {
+  description = "SNS Topic ARN for notifications"
+  value       = aws_sns_topic.order_confirmation.arn
+}
