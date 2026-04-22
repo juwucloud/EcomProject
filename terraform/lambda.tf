9c8248efa0c4f5c5b13f0dccf2d1173b461118d4
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2026-04-22 14:32:35 +0200

    added environment variables to order intake for testing in console

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,17 +1,25 @@
 data "archive_file" "order_intake" {
-    type        = "zip"
-    source_file = "${path.module}/../LambdaFunctions/OrderIntake.py"
-    output_path = "${path.module}/lambda_zips/order_intake.zip"
+  type        = "zip"
+  source_file = "${path.module}/../LambdaFunctions/OrderIntake.py"
+  output_path = "${path.module}/lambda_zips/order_intake.zip"
 }
 
 resource "aws_lambda_function" "order_intake" {
-    filename = data.archive_file.order_intake.output_path
-    function_name = "order-intake"
-    role = aws_iam_role.lambda_execution_role.arn
-    handler = "OrderIntake.lambda_handler"
-    runtime = "python3.12"
-    source_code_hash = data.archive_file.order_intake.output_base64sha256
+  filename         = data.archive_file.order_intake.output_path
+  function_name    = "order-intake"
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "OrderIntake.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = data.archive_file.order_intake.output_base64sha256
+
+  environment {
+    variables = {
+      ORDERS_TABLE    = aws_dynamodb_table.orders.name
+      ORDER_QUEUE_URL = aws_sqs_queue.order_processor_queue.url
+    }
+  }
 }
+
 
 
 
